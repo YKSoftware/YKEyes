807f9d971732d85cd5e3f25b62937f67391152da
YKSoftware <wakuwakuwaffurunrun@outlook.jp>
2019-11-22 18:47:47 +0900

    Eye ウィンドウクラスを追加しました。

---
+++ source/YKEyes/Views/Eye.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace YKEyes.Views
+{
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Eye.xaml の相互作用ロジック
+    /// </summary>
+    public partial class Eye : UserControl
+    {
+        public Eye()
+        {
+            InitializeComponent();
+        }
+    }
+}
