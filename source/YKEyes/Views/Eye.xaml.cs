2c43e98e4b46616a4d7380e1ba8a0593869c0da7
YKSoftware <wakuwakuwaffurunrun@outlook.jp>
2019-11-22 20:20:21 +0900

    マウスフックし、中心座標とマウス座標を取得できるようにしました。

--- source/YKEyes/Views/Eye.xaml.cs
+++ source/YKEyes/Views/Eye.xaml.cs
@@ -1,5 +1,7 @@
 ﻿namespace YKEyes.Views
 {
+    using System.ComponentModel;
+    using System.Windows;
     using System.Windows.Controls;
 
     /// <summary>
@@ -7,9 +9,41 @@
     /// </summary>
     public partial class Eye : UserControl
     {
+        /// <summary>
+        /// 新しいインスタンスを生成します。
+        /// </summary>
         public Eye()
         {
             InitializeComponent();
+
+            // デザイン実行時に this.ActualWidth などにアクセスするとデザイナーで以下の例外が発生します。
+            // System.InvalidOperationException
+            //   この Visual は、PresentationSource に接続されていません。
+            // このため、そもそも問題が発生しないようにマウスフックさせないようにします。
+            if (!DesignerProperties.GetIsInDesignMode(this))
+            {
+                this._mouseHook.MouseMove += MouseMoveHook;
+                this._mouseHook.Hook();
+            }
         }
+
+        /// <summary>
+        /// MouseMove をフックします。
+        /// </summary>
+        /// <param name="e">イベント引数</param>
+        private void MouseMoveHook(YKToolkit.Controls.MouseHook.MSLLHOOKSTRUCT e)
+        {
+            // 自分の中心座標
+            var origin = this.PointToScreen(new Point(this.ActualWidth / 2, this.ActualHeight / 2));
+            System.Diagnostics.Debug.WriteLine("中心 = " + origin);
+
+            // マウス座標
+            System.Diagnostics.Debug.WriteLine("マウス = " + e.Point);
+        }
+
+        /// <summary>
+        /// マウスフックするためのオブジェクト
+        /// </summary>
+        private YKToolkit.Controls.MouseHook _mouseHook = new YKToolkit.Controls.MouseHook();
     }
 }
